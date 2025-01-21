48cb4efff07e09afdbebb0714ec83010c404d60c
縣陵探究ラボ <148528521+kenryo-steam-lab@users.noreply.github.com>
2025-01-21 20:54:03 +0900

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -107,13 +107,8 @@ class HomePageState extends State<HomePage> {
           ),
         ),
         TextButton.icon(
-          onPressed: ()async{Navigator.of(context)
-            .pushAndRemoveUntil(AuthenticationPage.route(), (route) => false);
-            Door = 'Door is Unlocked.';
-            await supabase
-              .from('action')
-              .insert({'text': 'Unlock Request','number':565656});
-          },
+          onPressed: Navigator.of(context)
+            .pushAndRemoveUntil(AuthenticationPage.route(), (route) => false),
           icon: const Icon(
             Icons.lock_open,
             color:Colors.blue,
