7f74fc306bc1e765761e767a2f938b90cb0f1a09
縣陵探究ラボ <148528521+kenryo-steam-lab@users.noreply.github.com>
2025-01-21 23:49:51 +0900

    Update authentication_page.dart

--- lib/pages/authentication_page.dart
+++ lib/pages/authentication_page.dart
@@ -78,7 +78,7 @@ class AuthenticationPageState extends State<AuthenticationPage> {
             // Send a message once the client is subscribed
             channelB.sendBroadcastMessage(
               event: 'RequestForUnlocking',
-              payload: {'payload': message, 'user':"myUserId"},
+              payload: {'payload': message, 'user':myUserId},
             );
             });
             print("45");
