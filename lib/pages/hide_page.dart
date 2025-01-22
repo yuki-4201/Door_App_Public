1946b2c5df0eae7eb1affe5d6e8cc28f89e9131f
縣陵探究ラボ <148528521+kenryo-steam-lab@users.noreply.github.com>
2025-01-22 12:28:49 +0900

    Update hide_page.dart

--- lib/pages/hide_page.dart
+++ lib/pages/hide_page.dart
@@ -78,6 +78,13 @@ class HidePageState extends State<HidePage> {
           },
         ),
         ListTile(
+          title: const Text('Kenyro Students HP'),
+          onTap: (){
+            final url = Uri.parse('https://student.kenryo.ed.jp');
+            launchUrl(url);
+          },
+        ),
+        ListTile(
           title: const Text('KenryoArchive(iOS)'),
           onTap: (){
             final url = Uri.parse('https://apps.apple.com/jp/app/%E7%B8%A3%E9%99%B5%E6%8E%A2%E7%A9%B6%E3%82%A2%E3%83%BC%E3%82%AB%E3%82%A4%E3%83%96/id6738385612');
